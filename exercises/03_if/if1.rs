2fee867b873e1324c8063eb72c0d12c485eb8f60
AhmedElshewemy <ahmedelshewemy11@gmail.com>
2025-09-02 20:02:24 +0300

    solve var and func and if

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -1,13 +1,15 @@
 fn bigger(a: i32, b: i32) -> i32 {
-    // TODO: Complete this function to return the bigger number!
-    // If both numbers are equal, any of them can be returned.
-    // Do not use:
-    // - another function call
-    // - additional variables
+    if a > b {
+        println!("{}", a);
+        a
+    } else {
+        println!("{}", b);
+        b
+    }
 }
 
 fn main() {
-    // You can optionally experiment here.
+   bigger(2, 3);
 }
 
 // Don't mind this for now :)

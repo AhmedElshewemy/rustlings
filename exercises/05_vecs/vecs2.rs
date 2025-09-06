e82ec2a7d4b259cdf850d5f8fbc300410214d523
AhmedElshewemy <ahmedelshewemy11@gmail.com>
2025-09-06 20:04:47 +0300

    vecs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
+
     }
 
     output
@@ -14,6 +16,7 @@ fn vec_map_example(input: &[i32]) -> Vec<i32> {
     // We map each element of the `input` slice to its value plus 1.
     // If the input is `[1, 2, 3]`, the output is `[2, 3, 4]`.
     input.iter().map(|element| element + 1).collect()
+
 }
 
 fn vec_map(input: &[i32]) -> Vec<i32> {
@@ -21,12 +24,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // by 2, but with iterator mapping instead of manually pushing into an empty
     // vector.
     // See the example in the function `vec_map_example` above.
-    input
-        .iter()
-        .map(|element| {
-            // ???
-        })
-        .collect()
+    input.iter().map(|element| {  element * 2 }).collect() // Multiply each element by 2
 }
 
 fn main() {
